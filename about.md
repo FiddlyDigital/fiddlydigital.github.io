8c15f7d0b61ca0107e1acd038c8b5338e21361c1
FiddlyDigital <fiddlydigital@gmail.com>
2018-02-13 23:27:50 +0000

    Removed Bootstrap, Replaced With Bulma. Rejigged site styling

--- about.md
+++ about.md
@@ -3,7 +3,7 @@ layout: fd_page
 title: About
 permalink: /about
 ---
-### About Me
+### Who I am
 I'm Phil, a FullStack Developer from Dublin, Ireland
 
 I'm extremely passionate about Technology and software.
@@ -13,32 +13,9 @@ I've been fortunate enough to work across a large breadth of the industry.
 I've worked in a lot of environments, from 6-people start-ups to 6000+ employee Enterprise corporations.
 Along the way I've worked on some amazing projects and learned from some true professionals.
 
-I extremely enjoy my vocation and hope my works and words here give back (even in a small way!) to the excellent development communities I've had the pleasure of being a part of.
+### What I do
 
-#### Current Project
+You can see more of what I'm up to on [Instagram][instagram] and [Github][github].
 
-[DbDoc][dbdoc] - a great tool for anyone who works with SQLServer. Check it out [here][dbdoc]
-
-#### Technologies I Use
-
-* Javascript -ES5 + ES6
-    * AngularJS
-    * VueJS
-    * Jquery
-    * NPM, Yarn, Bower
-    * Gulp, WebPack
-* C# &amp; .Net
-    * ASP.Net MVC / WebAPI 
-    * MSMQ
-    * Entity Framework
-* SQLServer
-    * T-SQL
-    * SMO
-* Standard Web Technologies
-    * HTML5, XHTML
-    * CSS 2/3, LESS
-* Data Formats
-    * JSON
-    * XML
-
-[dbdoc]: /dbdoc
+[instagram]: https://www.instagram.com/fiddlydigital
+[github]: https://github.com/fiddlydigital
--- js/site.js
+++ js/site.js
@@ -1,38 +1,13 @@
-/* global window,$ */
-var $posts = null;
-var $links = null;
-
-function filterPosts(filter) {
-    $links.children().removeClass('active');
-    
-    if(filter == "all"){
-        $posts.children().removeClass('hidden');
-        
+document.addEventListener('DOMContentLoaded', function () {
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    if ($navbarBurgers.length > 0) {
+        $navbarBurgers.forEach(function ($el) {
+            $el.addEventListener('click', function () {
+                var target = $el.dataset.target;
+                var $target = document.getElementById(target);
+                $el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+            });
+        });
     }
-    else{
-        $posts.children().addClass('hidden');
-        $posts.find('[data-category="' + filter +'"]').removeClass('hidden');
-    }
-    
-    $links.find('[data-type="' + filter + '"]').addClass('active');
-};
-
-function checkHash() {
-    var hash = window.location.hash;
-    
-    if(!hash){
-        hash = '#all'
-    }
-    
-    filterPosts(hash.substring(1, hash.length));
-};
-
-$( document ).ready(function() {
-    $posts = $('#posts');
-    $links = $('.categories');
-    
-    window.addEventListener("hashchange", checkHash, false);
-    
-    checkHash();
 });
-
