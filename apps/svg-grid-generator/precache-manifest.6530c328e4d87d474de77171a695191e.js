533b38e1491d9f1cbd2a6b9a9d683edde330467b
FiddlyDigital <fiddlydigital@gmail.com>
2018-07-11 14:10:40 +0100

    Updated Build of Grid Generator

---
+++ apps/svg-grid-generator/precache-manifest.6530c328e4d87d474de77171a695191e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "33b867443f35b4cfba5d",
+    "url": "/js/vendors~app.33b86744.js"
+  },
+  {
+    "revision": "f44989740ca2f7bc3350",
+    "url": "/js/app.f4498974.js"
+  },
+  {
+    "revision": "27949fe7f19a3eecfa57108c2281dbf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f44989740ca2f7bc3350",
+    "url": "/css/app.d99fffe7.css"
+  }
+];
--- apps/svg-grid-generator/service-worker.js
+++ apps/svg-grid-generator/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5b16ca7f58b552fe1a79ef8beeced4cc.js"
+  "/precache-manifest.6530c328e4d87d474de77171a695191e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "svg-grid-generator"});
